36ab5d9c2b9f99bb3f2a004342bcd59b11558e64
vannn-tech <jovan20166@gmail.com>
2026-05-12 19:36:23 +0700

    menambahkan koneksi db

--- FormRegister.cs
+++ FormRegister.cs
@@ -6,7 +6,7 @@ namespace ReservasiFutsal02
 {
     public partial class FormRegister : Form // Form untuk registrasi akun baru, hanya untuk role 'User'
     {
-        string connectionString = @"Data Source=LAPTOP-5R80O1Q5\MSSQLSERVER01;Initial Catalog=DBFutsalADO;Integrated Security=True";
+        string connectionString = @"Data Source=LAPTOP-5R80O1Q5\MSSQLSERVER01;Initial Catalog=DBFutsalADO;Integrated Security=True"; // Ganti dengan string koneksi yang sesuai dengan database Anda 
 
         public FormRegister()
         {
